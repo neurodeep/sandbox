e84c2a3755ad3ade7c2036d5eae087b3553f292e
neurodeep <neurodeep@gmail.com>
2018-04-07 08:32:30 +0300

    add metadata

--- youtube-fullscreen-like/script.js
+++ youtube-fullscreen-like/script.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @namespace     https://openuserjs.org/users/neurodeep
+// @name          Getting Started with a User Script
+// @description   Showing the current basic and recommended format for a User script.
+// @copyright     2018, neurodeep (https://openuserjs.org/users/neurodeep)
+// @license       OSI-SPDX-Short-Identifier
+// @version       0.0.0
+// @include       https://https://www.youtube.com/watch*
+// @grant none
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author neurodeep
+// ==/OpenUserJS==
+
 // Continuos Integration
 
 // Adds Like/Dislike buttons and keyboard shortcuts to fullscreen.
